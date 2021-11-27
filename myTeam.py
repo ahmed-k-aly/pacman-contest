5f04fa5a4d0b8e6b9af47fdd797e5f3e4b431c90
ahmed-k-aly <ahmedkaly1101@gmail.com>
2021-11-27 16:53:19 -0500

    s

--- myTeam.py
+++ myTeam.py
@@ -14,7 +14,7 @@
 
 from captureAgents import CaptureAgent
 import random, time, util
-from game import Directions
+from game import Actions, Directions
 import game
 
 #################
@@ -72,7 +72,6 @@ class DummyAgent(CaptureAgent):
     CaptureAgent.registerInitialState in captureAgents.py.
     '''
     CaptureAgent.registerInitialState(self, gameState)
-    print("sasa")
     '''
     Your initialization code goes here, if you need any.
     '''
@@ -87,6 +86,103 @@ class DummyAgent(CaptureAgent):
     '''
     You should change this in your own agent.
     '''
+    surroundingState = util.Counter()
+    for action in actions:
+      newGameState = gameState.generateSuccessor(self.index, action)
+      numFoodLeft = len(self.getFood(newGameState).asList())
+      stateValue = -self.distanceToExtremeFood(newGameState,self.getFood(newGameState).asList()) + (100*self.ghostFunction(newGameState)) - 100*numFoodLeft
+      print("Action, StateValue = {}, {}".format(action, stateValue))
+      surroundingState[action] = stateValue
+      
+    return surroundingState.argMax()
 
-    return random.choice(actions)
+  def ghostFunction(self, gameState):
+    ''' 
+    Evaluates the state relative to the agents of the opponents.
+    Returns a value from 0-1 based on how good the current state is
+    relative only to ghosts
+    '''
+    #TODO: Method for power pellets logic
+    #TODO: Make logic for when we're scared ghost and opponent is PACMAN
+    #TODO: Make logic for when we're PACMAN and opponent is scared ghost
+    
+    enemyIndices = self.getOpponents(gameState)
+    enemyDistances = util.Counter()
+    enemiesStates = util.Counter()
+    utilityEstimate = {} # how scared am I from each opponent
+    myState = self.agentState(gameState, self.index)
+    for i in enemyIndices:
+      utilityEstimate[i] = 0
+      enemyDistances[i] = self.getEnemyDistance(gameState, i)
+      enemiesStates[i] = self.agentState(gameState, i)
+    for i in enemyIndices:
+      # TODO: add power pellet logic
+      if enemiesStates[i] == "Ghost" and myState == "Pacman":
+        # Try and get away from the enemy
+          if enemyDistances[i] != None:
+            utilityEstimate[i] = enemyDistances[i]
+          elif enemyDistances[i] == None:
+            utilityEstimate[i] = 1
+          elif enemyDistances[i] == 2 or enemyDistances[i] == 1: 
+            print("enemyDistance: {}".format(enemyDistances[i]))
+            utilityEstimate[i] = float('-inf')
+      elif enemiesStates[i] == "Pacman" and myState == "Ghost":
+        # Try and approach the enemy
+          if enemyDistances[i] != None:
+            utilityEstimate[i] = -enemyDistances[i]
+          elif enemyDistances[i] == None: 
+            #TODO: ADD prob distribution logic
+            # Hunt Pacman
+            utilityEstimate[i] = 0
+    return min(utilityEstimate.values())
 
+
+  def agentState(self,gameState, agentIndex):
+    ''' 
+    Returns if the passed in Agent is ghost or PACMAN
+    '''
+    agentState = str(gameState.getAgentState(agentIndex))
+    return agentState.split(':')[0]
+
+  def distanceToExtremeFood(self, gameState, food, isClosest = True):
+      myPos = gameState.getAgentPosition(self.index) 
+      foodDistances = []
+      for foodPos in food:
+        foodDistances.append(self.getMazeDistance(foodPos, myPos))
+      if isClosest:
+        return min(foodDistances)
+      else:
+        return max(foodDistances)
+  
+  def getEnemyDistance(self, gameState, enemyIndex):
+    ''' 
+    Returns Enemy distance if within our range. Else, return None
+    '''
+    myPos = gameState.getAgentPosition(self.index)
+    enemyPos =  gameState.getAgentPosition(enemyIndex)
+    if myPos and enemyPos != None:
+      distance = self.getMazeDistance(myPos,enemyPos)
+      return distance
+    return None
+  
+  def closestFood(self, pos, food, walls):
+    """
+    closestFood -- this is similar to the function that we have
+    worked on in the search project; here its all in one place
+    """
+    fringe = [(pos[0], pos[1], 0)]
+    expanded = set()
+    while fringe:
+        pos_x, pos_y, dist = fringe.pop(0)
+        if (pos_x, pos_y) in expanded:
+            continue
+        expanded.add((pos_x, pos_y))
+        # if we find a food at this location then exit
+        if food[pos_x][pos_y]:
+            return dist
+        # otherwise spread out from the location to its neighbours
+        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+        for nbr_x, nbr_y in nbrs:
+            fringe.append((nbr_x, nbr_y, dist+1))
+    # no food found
+    return None
